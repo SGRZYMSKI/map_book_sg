fe4a80817efc540ac9fb300ec913688b72d86a89
SGRZYMSKI <167193699+SGRZYMSKI@users.noreply.github.com>
2024-04-23 14:25:57 +0200

    feat: create was added

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from models.data import users
-from utils.crud import read
-
+from utils.crud import read, add_user
 
 if __name__ == '__main__':
     print(f'witaj {users[0]["name"]}')
 
-
     read(users)
-
+    add_user(users)
+    read(users)
--- utils/crud.py
+++ utils/crud.py
@@ -1,3 +1,10 @@
 def read(users: list[dict[str, str]])->None:
     for user in users[1:]:
         print(f'twój znajomy {user["name"]} opublikował {user["post"]} posty ')
+
+def add_user(lista: list) -> None:
+    user_name = input('Podaj imie: ')
+    user_surname = input('podaj nazwisko: ')
+    user_post = input('podaj ile postów: ')
+    lista.append({'name': user_name, 'surname': user_surname, 'post': user_post})
+
